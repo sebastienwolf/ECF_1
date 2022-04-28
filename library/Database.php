8913afddf5432a3507e492e05de28e1a44ae1bf9
sebastienwolf <s.mouret.pro@gmail.com>
2022-04-28 23:17:25 +0200

    connection visble pour ECF

--- library/Database.php
+++ library/Database.php
@@ -9,7 +9,23 @@ class Database
 
     public static function getPdo(): PDO
     {
-        @include_once "./library/connection.php";
+        //Il est en commentaire car la véritable connection pour mon site en ligne ce trouve dans ce dossier
+        // et je le met dans le gitignore pour ne pas qu'il sois prit sur github
+        // cela protege ma BDD
+        //@include_once "./library/connection.php";
+
+        $dbHost = "localhost";
+        $dbName = "SMMediaBdd";
+        $dbUser = "sebastien";
+        $dbPwd = "sebastien";
+
+        if (self::$instance === null) {
+            self::$instance = $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8", "$dbUser", "$dbPwd", [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        }
+
 
         return self::$instance;
     }
